057e36f8e062f027ae6bef5eae155a5770042a6c
Patrick Krueger <sendthistopatrick@gmail.com>
2023-01-31 09:15:26 -0700

    Drag and drop

---
+++ components/Card.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react"
+import { useSortable } from "@dnd-kit/sortable"
+import { CSS } from "@dnd-kit/utilities"
+
+type Props = {
+  id: string
+  image:string
+}
+
+export default function Card({ id, image }: Props) {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition
+  } = useSortable({ id })
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition
+  }
+
+  return (
+    <div className="card" ref={setNodeRef} style={style} {...attributes} {...listeners}>
+      <img className="photo-card" src={image} alt="some house" />
+    </div>
+  )
+}
